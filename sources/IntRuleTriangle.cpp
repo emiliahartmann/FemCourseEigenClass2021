c2e09a91cedc88559054bc7f0dddebd6b68df9d8
Emilia Hartmann <geof_emilia@outlook.com>
2021-05-11 08:10:43 -0300

    Versão de pontos e pesos certos para o IntRuleQuad.

--- sources/IntRuleTriangle.cpp
+++ sources/IntRuleTriangle.cpp
@@ -13,7 +13,8 @@ IntRuleTriangle::IntRuleTriangle(){
 }
 
 IntRuleTriangle::IntRuleTriangle(int order) {
-    DebugStop();
+    SetOrder(order);
+//    DebugStop();
 }
 
 void IntRuleTriangle::SetOrder(int order) {
@@ -21,13 +22,35 @@ void IntRuleTriangle::SetOrder(int order) {
     if(order < 0 || order > MaxOrder());
     switch (order){
         case 0:
-            DebugStop();
-            break;
         case 1:
+//            fPoints.resize(1,Dimension());
+//            fWeights.resize(1);
+//            fPoints(0,0) =  1./3; 
+//            fPoints(0,1) =  1./3;
+//            fWeights(0) = 1.;
+            fPoints.resize(3,Dimension());
+            fWeights.resize(3);
+            fPoints(0,0) =  -1./3.;    
+            fPoints(1,0) =  1./3;
+            fPoints(0,1) =  0.;    
+            fPoints(1,1) =  0.;
+            fWeights(0) = 1./3;
+            fWeights(1) = 1./3;
             DebugStop();
             break;
         case 2:
-            DebugStop();
+        case 3:
+            fPoints.resize(3,Dimension());
+            fWeights.resize(3);
+            fPoints(0,0) =  0.;    
+            fPoints(1,0) =  0.;
+            fPoints(2,0) =  1.;
+            fPoints(0,1) =  0.;    
+            fPoints(1,1) =  0.;
+            fPoints(2,1) =  1.;
+            fWeights(0) = 1./3;
+            fWeights(1) = 1./3;
+            fWeights(2) = 1./3;
             break;
         default:
             DebugStop();
