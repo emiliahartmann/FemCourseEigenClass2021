db107a09ffdec972d873d21f0cdc53c1acc40886
Emilia Hartmann <geof_emilia@outlook.com>
2021-05-19 13:30:14 -0300

    Completando o caso 4 e 5 para a integração triangular.

--- sources/IntRuleTriangle.cpp
+++ sources/IntRuleTriangle.cpp
@@ -76,30 +76,83 @@ void IntRuleTriangle::SetOrder(int order) {
             fWeights(2) = 25./96;
             fWeights(3) = 25./96;
             break;
-        case 4:  
-        case 5:
+        case 4: 
             fPoints.resize(6,Dimension());
             fWeights.resize(6);
-            fPoints(0,0) = 949./1440;    
-            fPoints(1,0) = 949./1440;
-            fPoints(2,0) = 613./2643;
-            fPoints(3,0) = 613./2643;
-            fPoints(4,0) = 573./5255;
-            fPoints(5,0) = 573./5255;
+            fPoints(0,0) = (8.-sqrt(10.)+sqrt(38.-44.*sqrt(2./5.)))/18.;    
+            fPoints(1,0) = (8.-sqrt(10.)+sqrt(38.-44.*sqrt(2./5.)))/18.;
+            fPoints(2,0) = 1.L-2.L*(8.-sqrt(10.)+sqrt(38.-44.*sqrt(2./5.)))/18.;
 
-            fPoints(0,1) = 613./2643;    
-            fPoints(1,1) = 573./5255;
-            fPoints(2,1) = 949./1440;
-            fPoints(3,1) = 573./5255;
-            fPoints(4,0) = 949./1440;
-            fPoints(5,0) = 613./2643;
+            fPoints(3,0) = (8.-sqrt(10.)-sqrt(38.-44.*sqrt(2./5.)))/18.;    
+            fPoints(4,0) = (8.-sqrt(10.)-sqrt(38.-44.*sqrt(2./5.)))/18.;
+            fPoints(5,0) = 1.L-2.L*(8.-sqrt(10.)-sqrt(38.-44.*sqrt(2./5.)))/18.;
 
-            fWeights(0) = 1./6;
-            fWeights(1) = 1./6;
-            fWeights(2) = 1./6;
-            fWeights(3) = 1./6;
-            fWeights(4) = 1./6;
-            fWeights(5) = 1./6;
+            fPoints(0,1) = (8.-sqrt(10.)+sqrt(38.-44.*sqrt(2./5.)))/18.;    
+            fPoints(1,1) = 1.L-2.L*(8.-sqrt(10.)+sqrt(38.-44.*sqrt(2./5.)))/18.;
+            fPoints(2,1) = (8.-sqrt(10.)+sqrt(38.-44.*sqrt(2./5.)))/18.;
+
+            fPoints(3,1) = (8.-sqrt(10.)-sqrt(38.-44.*sqrt(2./5.)))/18.;
+            fPoints(4,1) = 1.L-2.L*(8.-sqrt(10.)-sqrt(38.-44.*sqrt(2./5.)))/18.;
+            fPoints(5,1) = (8.-sqrt(10.)-sqrt(38.-44.*sqrt(2./5.)))/18.;
+
+            fWeights(0) = ((620. + sqrt(213125. - 53320. * sqrt(10.))) / 3720.)/2.;
+            fWeights(1) = ((620. + sqrt(213125. - 53320. * sqrt(10.))) / 3720.)/2.;
+            fWeights(2) = ((620. + sqrt(213125. - 53320. * sqrt(10.))) / 3720.)/2.;
+            fWeights(3) = ((620. - sqrt(213125. - 53320. * sqrt(10.))) / 3720.)/2.;
+            fWeights(4) = ((620. - sqrt(213125. - 53320. * sqrt(10.))) / 3720.)/2.;
+            fWeights(5) = ((620. - sqrt(213125. - 53320. * sqrt(10.))) / 3720.)/2.; 
+
+//            fPoints.resize(6,Dimension());
+//            fWeights.resize(6);
+//            fPoints(0,0) = 949./1440;    
+//            fPoints(1,0) = 949./1440;
+//            fPoints(2,0) = 613./2643;
+//            fPoints(3,0) = 613./2643;
+//            fPoints(4,0) = 573./5255;
+//            fPoints(5,0) = 573./5255;
+
+//            fPoints(0,1) = 613./2643;    
+//            fPoints(1,1) = 573./5255;
+//            fPoints(2,1) = 949./1440;
+//            fPoints(3,1) = 573./5255;
+//            fPoints(4,1) = 949./1440;
+//            fPoints(5,1) = 613./2643;
+
+//            fWeights(0) = 1./6;
+//            fWeights(1) = 1./6;
+//            fWeights(2) = 1./6;
+//            fWeights(3) = 1./6;
+//            fWeights(4) = 1./6;
+//            fWeights(5) = 1./6; 
+            break;
+        case 5:
+            fPoints.resize(7,Dimension());
+            fWeights.resize(7);
+            fPoints(0,0) = (6.-sqrt(15.))/21.;    
+            fPoints(1,0) = (6.-sqrt(15.))/21.;
+            fPoints(2,0) = 1.L-2.0L*(6.-sqrt(15.))/21.;
+
+            fPoints(3,0) = (6.+sqrt(15.))/21.;
+            fPoints(4,0) = (6.+sqrt(15.))/21.;
+            fPoints(5,0) = 1.L-2.0L*(6.+sqrt(15.))/21.;
+            fPoints(6,0) = 1./3.;
+
+            fPoints(0,1) = (6.-sqrt(15.))/21.;   
+            fPoints(1,1) = 1.L-2.0L*(6.-sqrt(15.))/21.;
+            fPoints(2,1) = (6.-sqrt(15.))/21.;
+
+            fPoints(3,1) = (6.+sqrt(15.))/21.;
+            fPoints(4,1) = 1.L-2.0L*(6.+sqrt(15.))/21.;
+            fPoints(5,1) = (6.+sqrt(15.))/21.;
+            fPoints(6,1) = 1./3.;
+
+            fWeights(0) = (155.-sqrt(15.))/2400.;
+            fWeights(1) = (155.-sqrt(15.))/2400.;
+            fWeights(2) = (155.-sqrt(15.))/2400.;
+            fWeights(3) = (155.+sqrt(15.))/2400.;
+            fWeights(4) = (155.+sqrt(15.))/2400.;
+            fWeights(5) = (155.+sqrt(15.))/2400.;
+            fWeights(6) = 9./80.;
             break;
 
         break;
