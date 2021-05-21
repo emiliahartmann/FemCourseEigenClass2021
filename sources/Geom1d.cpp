043fe2600cbaa802873c0b4d220553eef7a57c80
Emilia Hartmann <geof_emilia@outlook.com>
2021-05-21 14:49:27 -0300

    Completando o mapeamento geométrico 1d e começando a construir a função shape do GeomQuad.

--- sources/Geom1d.cpp
+++ sources/Geom1d.cpp
@@ -21,22 +21,55 @@ Geom1d& Geom1d::operator=(const Geom1d& copy) {
 }
 
 void Geom1d::Shape(const VecDouble &xi, VecDouble &phi, MatrixDouble &dphi) {
-//    xi.resize(2);
-    phi.resize(2);
-//    phi(0) = (1.-xi)/2;
-//    phi(1) = (1.+xi)/2;
-    dphi.resize(1,2);
-//    dphi(0,1) = -1./2;
-//    dphi(1,1) = 1./2;
-//    DebugStop();
+    if(xi.size() !=Dimension || phi.size() !=nCorners || dphi.rows() !=Dimension || dphi.cols() !=nCorners)
+        DebugStop();
+    double csi = xi[0];
+
+    phi[0] = (1. - csi)/2.;
+    phi[1] = (1. + csi)/2.;
+
+    dphi(0,0) = -1./2.;
+    dphi(0,1) =  1./2.;
+
 }
 
 void Geom1d::X(const VecDouble &xi, MatrixDouble &NodeCo, VecDouble &x) {
-    DebugStop();
+    if(xi.size() != Dimension) DebugStop();
+    if(x.size() !=NodeCo.rows()) DebugStop();
+    if(NodeCo.cols() !=nCorners) DebugStop();
+    int nrow=NodeCo.rows();
+
+    VecDouble phi(2);
+    MatrixDouble dphi(Dimension,2);
+    Shape(xi, phi, dphi);
+
+    for (int i=0; i < nrow; i++) {
+        x[i] = NodeCo(i,0)*phi[0] + NodeCo(i,1)*phi[1];
+//        x[i] = NodeCo(i,0)*(1.-xi[0])*(1./2.) + NodeCo(i,1)*(1. + xi[0])*1./2.;
+    }
+//    DebugStop(); 
 }
 
 void Geom1d::GradX(const VecDouble &xi, MatrixDouble &NodeCo, VecDouble &x, MatrixDouble &gradx) {
-    DebugStop();
+    if(xi.size() != Dimension) DebugStop();
+    if(x.size() !=NodeCo.rows()) DebugStop();
+    if(NodeCo.cols() !=nCorners) DebugStop();
+    int nrow=NodeCo.rows();
+    int ncol=NodeCo.cols();
+
+    gradx.resize(nrow,1);
+    gradx.setZero();
+
+    VecDouble phi(2);
+    MatrixDouble dphi(Dimension,2);
+    Shape(xi, phi, dphi);
+    X(xi, NodeCo, x);       //  nao acho q que precisa calcular x de novo
+    for(int i = 0; i < ncol; i++){
+        for(int j = 0; j < nrow; j++){
+            gradx(j, 0) += NodeCo(j, i) * dphi(0, i);       
+        } 
+    }
+//    DebugStop();
 }
 
 void Geom1d::SetNodes(const VecInt &nodes) {
